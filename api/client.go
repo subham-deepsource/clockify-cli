988810e85e5a87ad147fac5cc23fe4c9b77809f2
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2020-08-26 08:45:03 -0300

    (feat): datetime is common

--- api/client.go
+++ api/client.go
@@ -15,10 +15,6 @@ import (
 // baseURL is the Clockify API base URL
 const baseURL = "https://api.clockify.me/api"
 
-type Logger interface {
-	Printf(string, ...interface{})
-}
-
 // Client will help to access Clockify API
 type Client struct {
 	*http.Client
@@ -181,8 +177,8 @@ func (c *Client) LogRange(p LogRangeParam) ([]dto.TimeEntry, error) {
 
 	b := true
 	filter := dto.TimeEntryStartEndRequest{
-		Start:    dto.DateTime{Time: p.FirstDate},
-		End:      dto.DateTime{Time: p.LastDate},
+		Start:    http.DateTime{Time: p.FirstDate},
+		End:      http.DateTime{Time: p.LastDate},
 		Hydrated: &b,
 	}
 
@@ -444,9 +440,9 @@ type CreateTimeEntryParam struct {
 func (c *Client) CreateTimeEntry(p CreateTimeEntryParam) (dto.TimeEntryImpl, error) {
 	var t dto.TimeEntryImpl
 
-	var end *dto.DateTime
+	var end *http.DateTime
 	if p.End != nil {
-		end = &dto.DateTime{Time: *p.End}
+		end = &http.DateTime{Time: *p.End}
 	}
 
 	r, err := c.NewRequest(
@@ -456,7 +452,7 @@ func (c *Client) CreateTimeEntry(p CreateTimeEntryParam) (dto.TimeEntryImpl, err
 			p.Workspace,
 		),
 		dto.CreateTimeEntryRequest{
-			Start:       dto.DateTime{Time: p.Start},
+			Start:       http.DateTime{Time: p.Start},
 			End:         end,
 			Billable:    p.Billable,
 			Description: p.Description,
@@ -572,7 +568,7 @@ func (c *Client) Out(p OutParam) error {
 			p.Workspace,
 		),
 		dto.OutTimeEntryRequest{
-			End: dto.DateTime{Time: p.End},
+			End: http.DateTime{Time: p.End},
 		},
 	)
 
@@ -601,9 +597,9 @@ type UpdateTimeEntryParam struct {
 func (c *Client) UpdateTimeEntry(p UpdateTimeEntryParam) (dto.TimeEntryImpl, error) {
 	var t dto.TimeEntryImpl
 
-	var end *dto.DateTime
+	var end *http.DateTime
 	if p.End != nil {
-		end = &dto.DateTime{Time: *p.End}
+		end = &http.DateTime{Time: *p.End}
 	}
 
 	r, err := c.NewRequest(
@@ -614,7 +610,7 @@ func (c *Client) UpdateTimeEntry(p UpdateTimeEntryParam) (dto.TimeEntryImpl, err
 			p.TimeEntryID,
 		),
 		dto.UpdateTimeEntryRequest{
-			Start:       dto.DateTime{Time: p.Start},
+			Start:       http.DateTime{Time: p.Start},
 			End:         end,
 			Billable:    p.Billable,
 			Description: p.Description,
